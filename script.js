4c7d00db289536a7faf809c5aac852a4d45661db
leticia2804 <135224747+leticia2804@users.noreply.github.com>
2024-03-31 18:39:43 -0300

    terceiro commit do JS

--- script.js
+++ script.js
@@ -32,5 +32,13 @@ botoesGorjeta.forEach(botao => {
 })
 
 function receberPorcentagem (evento) {
-    console.log(evento.target.value)
+botoesGorjeta.forEach(botao => {
+    botao.classList.remove("botao-ativo")
+
+    if(botao.value === evento.target.value) {
+        botao.classList.add("botao-ativo")
+    }
+})
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
